764556ca0187beb1170597c02f334788630006b0
GabrielLoureiro <56445868+GabrielLoureiro@users.noreply.github.com>
2020-05-29 14:20:56 -0300

    Add files via upload

---
+++ Testando Javascript/main.js
@@ -0,0 +1,9 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    
+}
+
+function redirecionar(){
+    window.open("https://www.google.com");
+    //window.location.href = "https://www.google.com";
+}
